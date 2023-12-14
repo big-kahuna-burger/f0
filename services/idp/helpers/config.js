47d36bdab3db6b7b30312bc8a392ff279159fea3
big-kahuna-burger <arandjel.sarenac@gmail.com>
2023-12-14 03:53:30 +0100

    don't throw

--- services/idp/helpers/config.js
+++ services/idp/helpers/config.js
@@ -5,7 +5,5 @@ const file = `${process.env.ENV ? process.env.ENV : ''}.env`
 
 const __dirname = desm(import.meta.url)
 const { parsed, error } = config({ path: path.resolve(__dirname, '..', file) })
-if (error) {
-  throw error
-}
+
 export { parsed, error }
